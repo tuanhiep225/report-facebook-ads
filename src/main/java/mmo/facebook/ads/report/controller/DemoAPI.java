de4d96db3367da911a6a57f57baec6c6e89c9add
tuanhiep225 <tuanhiep225@gmail.com>
2019-05-21 15:39:51 +0700

    thêm demo

--- src/main/java/mmo/facebook/ads/report/controller/DemoAPI.java
+++ src/main/java/mmo/facebook/ads/report/controller/DemoAPI.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Version;
+import com.restfb.types.User;
+
 /**
  * @author Admin
  *
@@ -16,8 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/report/demo")
 public class DemoAPI {
+	final String MY_ACCESS_TOKEN = "EAACtyQLitBYBAJzCeMABmqM91ZCvnShRnxaZAZAcxICbsGffQZBeGuNdMgKafLwyIYZBSLQAau8KpADCrPC6eTds83kpwMTSQBZC1jm826FfUcWx9CZBtO6jLktpiDzHWIePJpq5OynRe5NZCpaIsDXEfDRGKjaZBVLJnbx5BvpMjtLrdZAluqsGXJMAE6bxZCURZCt3bWNJFE2QLXDEc9MGEV0x";
 	@GetMapping("/get-name")
-	public String getName() {
-		return "Hello";
+	public User getName() {
+		FacebookClient facebookClient = new DefaultFacebookClient(MY_ACCESS_TOKEN, Version.VERSION_3_1);
+		 User user = facebookClient.fetchObject("me", User.class);
+         
+	        System.out.println("User="+ user);
+	        System.out.println("UserName= "+ user.getUsername());
+	        System.out.println("Birthday= "+ user.getBirthday());
+	        return user;
 	}
 }
