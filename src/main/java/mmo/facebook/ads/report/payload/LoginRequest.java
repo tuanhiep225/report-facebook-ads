96bd121b4e6b8d8d4b4a0f27414bf6fd80d4574e
tuanhiep225 <tuanhiep225@gmail.com>
2019-06-04 15:07:57 +0700

    ignore properties Loginrequest

--- src/main/java/mmo/facebook/ads/report/payload/LoginRequest.java
+++ src/main/java/mmo/facebook/ads/report/payload/LoginRequest.java
@@ -3,9 +3,12 @@ package mmo.facebook.ads.report.payload;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by rajeevkumarsingh on 02/08/17.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LoginRequest {
     @NotBlank
     @Email
